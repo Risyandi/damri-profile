38f3e8e173f53c1580da3acc0f552c3bd6063df9
Risyandi <Risyandi.94@gmail.com>
2019-12-20 21:19:09 +0700

    modified

--- application/config/config.php
+++ application/config/config.php
@@ -42,7 +42,7 @@ $config['base_url'] = 'http://localhost/damri-profile/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -331,7 +331,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'damriprofilesites';
 
 /*
 |--------------------------------------------------------------------------
