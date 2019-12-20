107a17a828f16db7ddad4a6795f3d339d0a3035d
Risyandi <Risyandi.94@gmail.com>
2019-12-20 23:55:02 +0700

    modified dashboard and login

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Damri Profile | <?php echo $title ?></title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+
+    <?php if ($title == "Dashboard") : ?>
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/dashboard.css">
+    <?php elseif ($title == "Login") : ?>
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/login.css">
+    <?php endif; ?>
 </head>
 
 <body>
-    <p>Damri Profile / <?php echo $title ?></a></p>
